80bd6928a6f93a364cddb5cc82c2e86e7c64e1f1
IngridNavarroA <ingrid.navarro.an@gmail.com>
2018-07-23 20:10:32 -0400

    rviz works with pointclouds

--- src/nodes/npy_node.py
+++ src/nodes/npy_node.py
@@ -14,9 +14,47 @@ from PIL import Image
 import rospy
 from sensor_msgs.msg import Image as ImageMsg
 from std_msgs.msg import Header
+from sensor_msgs.msg import PointCloud2
+import sensor_msgs.point_cloud2 as pc2
 
 def _normalize(x):
     return (x - x.min()) / (x.max() - x.min())
+
+def _make_point_field(num_field):
+    msg_pf1 = pc2.PointField()
+    msg_pf1.name = np.str('x')
+    msg_pf1.offset = np.uint32(0)
+    msg_pf1.datatype = np.uint8(7)
+    msg_pf1.count = np.uint32(1)
+
+    msg_pf2 = pc2.PointField()
+    msg_pf2.name = np.str('y')
+    msg_pf2.offset = np.uint32(4)
+    msg_pf2.datatype = np.uint8(7)
+    msg_pf2.count = np.uint32(1)
+
+    msg_pf3 = pc2.PointField()
+    msg_pf3.name = np.str('z')
+    msg_pf3.offset = np.uint32(8)
+    msg_pf3.datatype = np.uint8(7)
+    msg_pf3.count = np.uint32(1)
+
+    msg_pf4 = pc2.PointField()
+    msg_pf4.name = np.str('intensity')
+    msg_pf4.offset = np.uint32(16)
+    msg_pf4.datatype = np.uint8(7)
+    msg_pf4.count = np.uint32(1)
+
+    #if num_field == 4:
+    #    return [msg_pf1, msg_pf2, msg_pf3, msg_pf4]
+
+    msg_pf5 = pc2.PointField()
+    msg_pf5.name = np.str('label')
+    msg_pf5.offset = np.uint32(20)
+    msg_pf5.datatype = np.uint8(4)
+    msg_pf5.count = np.uint32(1)
+
+    return [msg_pf1, msg_pf2, msg_pf3, msg_pf4, msg_pf5]
 
 class ImageConverter(object):
     """
@@ -42,8 +80,7 @@ class ImageConverter(object):
         rosimage = ImageMsg()
         rosimage.encoding = ImageConverter._ENCODINGMAP_PY_TO_ROS[img.mode]
         (rosimage.width, rosimage.height) = img.size
-        rosimage.step = (ImageConverter._PIL_MODE_CHANNELS[img.mode]
-                         * rosimage.width)
+        rosimage.step = (ImageConverter._PIL_MODE_CHANNELS[img.mode] * rosimage.width)
         rosimage.data = img.tobytes()
         return rosimage
 
@@ -74,7 +111,9 @@ class TrainingSetNode(object):
     def __init__(self, dataset_path='./data/lidar_2d',
                  pub_rate=10,
                  pub_feature_map_topic='/squeeze_seg/feature_map',
-                 pub_label_map_topic='/squeeze_seg/label_map'):
+                 pub_label_map_topic='/squeeze_seg/label_map',
+                 sub_topic='/kitti/points_raw',
+                 pub_topic='/squeeze_seg/points'):
         """
         ros node spin in init function
 
@@ -87,6 +126,7 @@ class TrainingSetNode(object):
         self._path = dataset_path + "/"
         self._pub_rate = pub_rate
         # publisher
+        self._pub = rospy.Publisher(pub_topic, PointCloud2, queue_size=1)
         self._feature_map_pub = rospy.Publisher(pub_feature_map_topic, ImageMsg, queue_size=1)
         self._label_map_pub = rospy.Publisher(pub_label_map_topic, ImageMsg, queue_size=1)
         # ros node init
@@ -149,6 +189,23 @@ class TrainingSetNode(object):
         lidar[np.where(label==2)] = [0., 1., 0., 0., 0.]
         lidar[np.where(label==3)] = [0., 1., 1., 0., 0.]
         lidar[np.where(label==3)] = [0., 0., 1., 0., 0.]
+
+        ## point cloud in filed of view
+        # x = np_p_ranged[:, 0].reshape(-1)
+        # y = np_p_ranged[:, 1].reshape(-1)
+        # z = np_p_ranged[:, 2].reshape(-1)
+        # i = np_p_ranged[:, 3].reshape(-1)
+        ## point cloud for SqueezeSeg segments
+        x = lidar[:, :, 0].reshape(-1)
+        y = lidar[:, :, 1].reshape(-1)
+        z = lidar[:, :, 2].reshape(-1)
+        i = lidar[:, :, 3].reshape(-1)
+        label = label.reshape(-1)
+        # cond = (label!=0)
+        # print(cond)
+        cloud = np.stack((x, y, z, i, label))
+
+
         # generated feature map from LiDAR data
         ##x/y/z
         # feature_map = Image.fromarray(
@@ -170,15 +227,26 @@ class TrainingSetNode(object):
         msg_label = ImageConverter.to_ros(label_map)
         msg_label.header = header
 
+        msg_segment = pc2.create_cloud(header=header,
+                                       fields=_make_point_field(cloud.shape[0]),
+                                       points=cloud.T)
+
+
         self._feature_map_pub.publish(msg_feature)
         self._label_map_pub.publish(msg_label)
-
+        self._pub.publish(msg_segment)
         file_name = img_file.strip('.npy').split('/')[-1]
         rospy.loginfo("%s published.", file_name)
 
 if __name__ == '__main__':
     # parse arguments from command line
-    parser = argparse.ArgumentParser(description='LiDAR point cloud 2D spherical surface publisher')
+    parser = argparse.ArgumentParser(description='LiDAR point cloud semantic segmentation')
+    parser.add_argument('--sub_topic', type=str,
+                        help='the pointcloud message topic to be subscribed, default `/kitti/points_raw`',
+                        default='/kitti/points_raw')
+    parser.add_argument('--pub_topic', type=str,
+                        help='the pointcloud message topic to be published, default `/squeeze_seg/points`',
+                        default='/squeeze_seg/points')
     parser.add_argument('--datasetPath', type=str,
                         help='the path of training dataset, default `./data/lidar_2d`',
                         default='./data/lidar_2d')
@@ -197,6 +265,7 @@ if __name__ == '__main__':
     node = TrainingSetNode(dataset_path=args.datasetPath,
                            pub_rate=args.pub_rate,
                            pub_feature_map_topic=args.pub_feature_map_topic,
-                           pub_label_map_topic=args.pub_label_map_topic)
+                           pub_label_map_topic=args.pub_label_map_topic, 
+                           pub_topic=args.pub_topic)
 
     rospy.logwarn('finished.')
